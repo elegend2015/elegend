babf8c4a73755f4b0350983edbd7d90fc551f6d1
elegend <elegend2015@gmail.com>
2015-09-14 17:13:37 +0530

    sign in page modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'static_pages#index'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  
+  match '/index', to: 'static_pages#index', via: 'get', as: :index
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
