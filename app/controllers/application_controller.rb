79d56aaa80ffbea7aa6b28144c9d1ffd96fdb19a
elegend <elegend2015@gmail.com>
2015-10-05 17:32:37 +0530

    terms_of_service filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   
   protect_from_forgery with: :exception
-  #before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:terms_of_service]
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  
-  #layout :layout_by_resource
+
 
   def after_sign_in_path_for(resource)
     #request.env['omniauth.origin'] || stored_location_for(resource) || index_path
@@ -18,14 +17,6 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-
-  # def layout_by_resource
-  #   if devise_controller?
-  #     "devise_layout"
-  #   else
-  #     "application"
-  #   end
-  # end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name,:last_name,:email,:password, :password_confirmation) }
